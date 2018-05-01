bdc8b5eece1f94b5ead77deacef3e15d17f71267
devnpal <devnpl@hotmail.com>
2018-05-01 09:36:07 +0200

    Dev and Archu ActionResult is added

--- WebApplication_Demo/Controllers/HomeController.cs
+++ WebApplication_Demo/Controllers/HomeController.cs
@@ -7,32 +7,55 @@ using System.Web.Mvc;
 namespace WebApplication_Demo.Controllers
 {
     public class HomeController : Controller
+
+
     {
+        public ActionResult Dev()
+        {
+            ViewBag.Message = "This is Dev view from Controller";
+            return View();
+
+        }
+
+        public ActionResult Archu()
+        {
+            ViewBag.title = "This page is dedicated to Archana Khatiwada";
+            return View();
+        }
         public ActionResult Index()
         {
-           
-           return View();
+            ViewBag.Title = "This is me Edited";
 
-            ViewBag.Title = "This is me";
-
+            return View();
 
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.Now it will be edited";
 
             ViewBag.Name = "Dev";
 
             return View();
-
-            
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Allusers()
+        {
+         ViewBag.Message = "This is the new allusers";
+
+            return View();
+        }
+
+        public ActionResult TestAction()
+        {
+            ViewBag.Message="This is the test Action";
             return View();
         }
     }
